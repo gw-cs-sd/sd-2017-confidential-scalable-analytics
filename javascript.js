827b5727671663a609dad9ee9f276e9ba1b77a9f
faisalharbi <harbi@gwmail.gwu.edu>
2017-05-10 17:57:35 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -15,7 +15,83 @@ $(document).ready(function(){
 		$("#loginType").val("company");
 	});
 
+	$("#closeDocumentImage").on("click", function(){
+		$("#documentContainerHidden").css({"display":"none"});
+	});
+
+	$(".rightContent").click(function(){
+		$("#companySuggestionBox").css({"display":"none"});
+	});
+
+	$("#uploadDocumentButton").click(function(){
+		$("#image").click();
+	});
+		
+	$("#image").on("change", function(){
+		addOverlay();
+		//$("#uploadDocumentSubmit").click();
+		var file = $("#image").prop('files')[0];
+		//ocrCall(file);
+	});
+
+	$("#submitVerificationButton").click(function(){
+		if($("#verifier").val() && $("#threshold").val() && $("#bankList li").length){
+			$("#bankList li").each(function(e){
+				if($("#bankNames").val()){
+					$("#bankNames").val($("#bankNames").val() + "," + $(this).text());
+				} else {
+					$("#bankNames").val($(this).text());
+				}
+				$("#newVerificationForm").submit();
+			});
+		} else {
+			alert("Please fill in all fields and upload a document.");
+			return false;
+		}
+	});
+
+	$("#documentContainerHidden").on("click", "#closeDocumentView", function(){
+		$("#documentContainerHidden").css({"display":"none"});
+		removeImageUnderlay();
+	});
+
+	$(".imageUnderlay").on("click", function(){
+		$("#documentContainerHidden").css({"display":"none"});
+		removeImageUnderlay();
+	});
+
+	$("#addBankButton").on("click", function(){
+		if($("#banks").val().trim()){
+			var bank = $("#banks").val();
+			$("#bankList").append("<li style='margin-left:16px;font-size:18px;color:#253240;'>" + bank + "</li>");
+			$("#banks").val("");
+		} else {
+			alert("Please enter a bank name.");
+		}
+	});
+
 });
+
+function addOverlay(){
+	$(".overlay").css({"display":"block"});
+}
+
+function removeOverlay(){
+	$(".overlay").css({"display":"none"});
+}
+
+function addImageUnderlay(){
+	$(".imageUnderlay").css({"display":"block"});
+}
+
+function removeImageUnderlay(){
+	$(".imageUnderlay").css({"display":"none"});
+}
+
+function selectCompanySuggestion(company){
+	$("#verifier").val(company);
+	$("#companySuggestionBox").css({"display":"none"});
+}
 
 function loadDynamicJNLP(){
 	$.ajax({
